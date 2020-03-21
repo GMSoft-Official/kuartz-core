8fdc49675cf99bc5c4470641addc2954d173880a
kutaycelebi <celebikutay@gmail.com>
2020-03-21 17:13:16 +0300

    - add label field to KzEnum.java for localization

--- kuartz-core-common/src/main/java/com/kuartz/core/common/enumaration/KzEnum.java
+++ kuartz-core-common/src/main/java/com/kuartz/core/common/enumaration/KzEnum.java
@@ -1,4 +1,12 @@
 package com.kuartz.core.common.enumaration;
 
 public interface KzEnum {
+
+    /**
+     * A field available for localization.
+     *
+     * @return {@link String}
+     */
+    String label();
+
 }
