83698e2f6970e04e744b7f5afbb3f8c0a33efc08
kutaycelebi <celebikutay@gmail.com>
2020-03-21 18:09:10 +0300

    - remove label field from KzEnum.java

--- kuartz-core-common/src/main/java/com/kuartz/core/common/enumaration/KzEnum.java
+++ kuartz-core-common/src/main/java/com/kuartz/core/common/enumaration/KzEnum.java
@@ -1,12 +1,4 @@
 package com.kuartz.core.common.enumaration;
 
 public interface KzEnum {
-
-    /**
-     * A field available for localization.
-     *
-     * @return {@link String}
-     */
-    String label();
-
 }
