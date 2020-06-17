812a1b1bbbab46ffd33721795694d2e50708cd31
kutay-celebi <celebikutay@gmail.com>
2020-06-17 19:24:02 +0300

    - added service package.

---
+++ kuartz-core-service/src/main/java/com/kuartz/core/service/KuartzService.java
@@ -0,0 +1,12 @@
+package com.kuartz.core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+
+public abstract class KuartzService {
+
+    @Autowired
+    private HttpServletRequest request;
+
+}
