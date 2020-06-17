731dddd9d5cd5c450844b4e9d3642d8a94da16cb
kutay-celebi <celebikutay@gmail.com>
2020-06-17 12:57:59 +0300

    - Added KzMessageModel

---
+++ kuartz-core-common/src/main/java/com/kuartz/core/common/model/KzMessageModel.java
@@ -0,0 +1,28 @@
+package com.kuartz.core.common.model;
+
+public class KzMessageModel {
+    private String message;
+
+    private Boolean success;
+
+    public KzMessageModel(String message, Boolean success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+}
